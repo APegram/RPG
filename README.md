2cfdc80a60d829c3beb3bc92ed45939996b30b52
Adam Pegram <adam@Adams-MacBook-Pro.local>
2019-05-03 15:51:08 -0400

    updated readme talking about modes and how to change

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ### RPG --- Automated Battle System ###
 
 Simply run game.js with node and it will select two random fighters to battle one another to the death. They may use potions and attack currently. More functionality to come!
+
+In game.js you can comment out various modes, and uncomment the lines you want to run. The Default mode is Battle Royale Free For All.
--- game.js
+++ game.js
@@ -11,10 +11,12 @@ let P5 = new Warrior('Fjore'.red, 'Tigran');
 
 let players = [P1, P2, P3, P4, P5]
 let player_amt = players.length
+let win_banner = `*********************************`.green
+let lose_banner = `**********************************`.red
 
-function fight(p1, p2){
-  p1.target = p2
-  p2.target = p1
+function fight(p1, p2, index, battle_type = '1v1'){
+  p1.target = p2 || null
+  p2.target = p1 || null
   let length = p1.skill_list.length
   let length2 = p2.skill_list.length
   let p1_skill = p1.skill_list[Math.floor(Math.random() * length)]
@@ -22,6 +24,10 @@ function fight(p1, p2){
 
   p1.cast(p1_skill)
   p2.cast(p2_skill)
+
+  if (index === (players.length - 1)){
+    players = players.filter(player => player.resources.health.current > 0)
+  }
 
   console.log(`
   **********************************************
@@ -36,29 +42,56 @@ function fight(p1, p2){
     } else if (p1.resources.health.current < 1) {
       console.log(`
       ${p2.name} has knocked out ${p1.name}!!
-      
-      ${p2.name} WINS!
       `)
     } else {
       console.log(`
       ${p1.name} has knocked out ${p2.name}!!
-      
-      ${p1.name} WINS!
       `)
     }
-    clearInterval(fighting)
+    if (battle_type === '1v1')
+      clearInterval(fighting)
+    
+    if (players.length === 1 && battle_type === 'BR'){
+      console.log(win_banner)
+      console.log(`* `.green + `${players[0].name} wins the Battle Royale!` + ` *`.green)
+      console.log(win_banner)
+      clearInterval(battleRoyaling)
+    } else if (players.length < 1 && battle_type === 'BR'){
+      console.log(lose_banner)
+      console.log(`No one survived the Battle Royale...`)
+      console.log(lose_banner)
+      clearInterval(battleRoyaling)
+    }
   }
 }
 
 
+function battleRoyale_fight(attacker, index) {
+  let targets = players.filter(player => player.name !== attacker.name)
+  let target_length = targets.length
+  fight(attacker, targets[Math.floor(Math.random() * target_length)], index, 'BR')
+}
 
-let player_one = players[Math.floor(Math.random()* player_amt)]
-let remaining_players = players.filter(player => {
-  if (player_one !== player) return player
-})
+function battleRoyale(attackers){
+  attackers.forEach((player, index) => {
+    battleRoyale_fight(player, index)
+  })
+}
 
-player_amt = remaining_players.length
-let player_two = remaining_players[Math.floor(Math.random() * player_amt)]
 
-let fighting = setInterval(() => {fight(player_one, player_two)}, 3000)
-fight(player_one, player_two)
+//******* Keep which mode you would like to run uncommented down below *****/
+//*** ONE VERSUS ONE ****/
+
+// let player_one = players[Math.floor(Math.random()* player_amt)]
+//   let remaining_players = players.filter(player => {
+//     if (player_one !== player) return player
+//   })
+//   player_amt = remaining_players.length
+//   let player_two = remaining_players[Math.floor(Math.random() * player_amt)]
+//   let fighting = setInterval(() => {fight(player_one, player_two)}, 3000)
+//   fight(player_one, player_two)
+
+//*** Battle Royale Free For All ***/
+
+battleRoyale(players);
+let battleRoyaling = setInterval(() => battleRoyale(players), 3000);
